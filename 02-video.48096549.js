304be8b5d383d5cd7509818b51b8f38f9674bda4
NadiiaSharafutdinova <111244855+NadiiaSharafutdinova@users.noreply.github.com>
2023-04-07 14:51:20 +0000

    Deploying to gh-pages from @ NadiiaSharafutdinova/hw-08@55a23c2519a8060c0cc7fb8ae187984a6bf08abe 🚀

---
+++ 02-video.48096549.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){n.currentTime}));
+//# sourceMappingURL=02-video.48096549.js.map
---
+++ 02-video.641cb293.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){n.currentTime}))}();
+//# sourceMappingURL=02-video.641cb293.js.map
