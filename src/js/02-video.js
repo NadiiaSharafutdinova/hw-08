55a23c2519a8060c0cc7fb8ae187984a6bf08abe
Nadiia Sharafutdinova <111244855+NadiiaSharafutdinova@users.noreply.github.com>
2023-04-07 16:50:40 +0200

    added feedback form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,13 @@
 
 const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
   
+ const onPlay = function()  {
+    const timeupdate = player.currentTime;
+    
+ }
+
+ player.on('play', onPlay);
+    
+
+   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,17 +2,17 @@ import throttle from "lodash.throttle";
 const STORAGE_KEY = 'feedback-msg';
 
 refs = {
-    email: document.querySelector('input'),
+    form: document.querySelector(".feedback-form"),
     message: document.querySelector('textarea'),
 };
 
-refs.email.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('submit', onFormSubmit);
 refs.message.addEventListener('input', throttle(onTextareaInput, 1000));
 
 function onTextareaInput(evt){
     const value = evt.currentTarget.value;
     console.log(value);
-    localStorage.setItem(STORAGE_KEY, refs.message);
+    localStorage.setItem(STORAGE_KEY, value);
 };
 
 function onFormSubmit(evt){
